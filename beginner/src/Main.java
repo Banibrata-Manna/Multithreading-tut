b25bd1d3845fb128ff15f4badd34526768ebe679
Banibrata Santanu Manna <banibrata.manna@hotwaxsystems.com>
2025-12-31 01:38:35 +0530

    Removed IDE util comment

--- beginner/src/Main.java
+++ beginner/src/Main.java
@@ -1,8 +1,6 @@
 import basics.MyFirstThread;
 import basics.MyRunnableThread;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome to the main thread!");
